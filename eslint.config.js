32565e866c968e37881418ba8363ab6d60222b91
fadhilrafiii <fadhil.muhammad@dansmultipro.com>
2024-08-20 21:15:27 +0700

    chore: initial config

---
+++ eslint.config.js
@@ -0,0 +1,78 @@
+import { fixupConfigRules, fixupPluginRules } from '@eslint/compat';
+import pluginJs from '@eslint/js';
+import typescriptParser from '@typescript-eslint/parser';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import pluginReact from 'eslint-plugin-react/configs/recommended.js';
+import pluginReactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import pluginSimpleImportSort from 'eslint-plugin-simple-import-sort';
+import pluginUnusedImports from 'eslint-plugin-unused-imports';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default [
+  {
+    languageOptions: {
+      globals: globals.browser,
+      parser: typescriptParser,
+    },
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...fixupConfigRules(pluginReact),
+  eslintConfigPrettier,
+  {
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  },
+  {
+    plugins: {
+      'react-hooks': fixupPluginRules(pluginReactHooks),
+      'react-refresh': fixupPluginRules(reactRefresh),
+      'unused-imports': pluginUnusedImports,
+      'simple-import-sort': pluginSimpleImportSort,
+    },
+    rules: {
+      ...pluginReactHooks.configs.recommended.rules,
+      'no-console': ['error', { allow: ['error', 'info'] }],
+      'prefer-const': 'warn',
+      'react/react-in-jsx-scope': 'off',
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+      'react/display-name': 'off',
+      'react/jsx-curly-brace-presence': [
+        'error',
+        {
+          props: 'never',
+          children: 'never',
+        },
+      ],
+      'unused-imports/no-unused-imports': 'error',
+      'unused-imports/no-unused-vars': [
+        'warn',
+        {
+          vars: 'all',
+          varsIgnorePattern: '^_',
+          args: 'after-used',
+          argsIgnorePattern: '^_',
+        },
+      ],
+      'simple-import-sort/imports': [
+        'error',
+        {
+          groups: [
+            ['^react$'],
+            ['^@?\\w'], // Third party
+            ['^@components([/].+)?$', '^@libs([/].+)?$'],
+            ['^\\.'], // Relative imports
+            ['.*.scss$'], // Assets
+            ['^@assets([/].+)?$'], // Assets
+          ],
+        },
+      ],
+    },
+  },
+];
